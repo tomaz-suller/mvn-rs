f00341ceb3ef9741c4cfa80b12863132697068c6
tomaz-suller <tomaz.suller@usp.br>
2023-03-01 09:52:21 -0300

    Add stdin reading helper function

--- mvn-utils/src/io.rs
+++ mvn-utils/src/io.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{io, fs};
 use std::path::{Path, PathBuf};
 
 pub fn file_exists(path: &str) -> Result<PathBuf, &'static str> {
@@ -14,5 +14,18 @@ pub fn file_exists(path: &str) -> Result<PathBuf, &'static str> {
 pub fn read_file_to_string(path: &PathBuf) -> String {
     fs::read_to_string(path)
         .expect("failed to read file")
+        .to_uppercase()
+}
+
+pub fn read_stdin_to_string() -> String {
+    io::stdin()
+        .lines()
+        .map(|result| result.expect("unable to read from stdin"))
+        .reduce(|mut acc, result| {
+            acc.push('\n');
+            acc.push_str(&result);
+            acc
+        })
+        .unwrap_or_default()
         .to_uppercase()
 }
