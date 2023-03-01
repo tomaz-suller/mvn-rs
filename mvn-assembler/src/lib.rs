bcde956aae19c020bf53ec2af632bf64900a061f
tomaz-suller <tomaz.suller@usp.br>
2023-03-01 12:21:33 -0300

    Extract assembler arg parser to to assembler crate
    The assembler binary now also has a `clap` interface, adapted to read
    either from a file passed using the `--input` or `-i` flag, or from
    `stdin` if no other input is set.

--- mvn-assembler/src/lib.rs
+++ mvn-assembler/src/lib.rs
@@ -2,3 +2,30 @@ pub mod processor;
 pub mod writer;
 
 pub use assembly::*;
+
+use std::path::PathBuf;
+
+use clap::Parser;
+use utils::io::{file_exists, read_file_to_string, read_stdin_to_string};
+use utils::Executor;
+
+use crate::{processor::process, writer::print};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, value_parser = file_exists)]
+    input: Option<PathBuf>,
+}
+
+impl Executor for Args {
+    fn execute(&self) {
+        let program = if let Some(path) = &self.input {
+            read_file_to_string(path)
+        } else {
+            read_stdin_to_string()
+        };
+        let process_result = process(&program);
+        print(&program, process_result);
+    }
+}
--- mvn-assembler/src/main.rs
+++ mvn-assembler/src/main.rs
@@ -1,19 +1,9 @@
-use std::io;
+use clap::Parser;
+use utils::Executor;
 
-use mvn_assembler::processor::process;
-use mvn_assembler::writer::print;
+use mvn_assembler::Args;
 
 fn main() {
-    let program = io::stdin()
-        .lines()
-        .map(|result| result.expect("unable to read from stdin"))
-        .reduce(|mut acc, result| {
-            acc.push('\n');
-            acc.push_str(&result);
-            acc
-        });
-    if let Some(program) = program {
-        let validator_output = process(&program);
-        print(&program, validator_output);
-    }
+    let args = Args::parse();
+    args.execute();
 }
